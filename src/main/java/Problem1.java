38402e4cb803fcb3c5a356298c4bffeba7eba9cc
Kengli Fu <kenglif12@gmail.com>
2020-10-07 18:57:37 -0700

    Problem1 done Tests for Problem2 mostly finished

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,8 +1,35 @@
 public class Problem1 {
-
+    private static int lowAdjust = 0;
+    private static int highAdjust = 0;
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
-        return -1; // Place holder
+
+        if (data != null && data.length >= 1) {
+
+            int high = data.length - 1 - highAdjust;
+            int low = lowAdjust;
+            //this finds what the middle is in a data set
+            int median = low + (high - low) / 2;
+
+            if (data[median] == target) {
+                lowAdjust = 0;
+                highAdjust = 0;
+                return median;
+
+            } else if (high == low) {
+                return -1;
+
+            } else if (target < data[median]) {
+                highAdjust = data.length - median;
+                return binarySearch(data, target);
+
+            } else if (target > data[median]) {
+                lowAdjust = median - low + 1;
+                return binarySearch(data, target);
+            }
+
+
+        } return -1;
     }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -1,14 +1,40 @@
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+//Bubble sort: ascending order
 public class Problem2Test {
 
     // Example test
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
         // add your tests here
+        int[][] inputs = {
+                {3},
+                {1, 2, 3},
+                {4, 9 , 0, 3, 6},
+                {7, -1, 12, 8},
+                {-10, -6, -18, -10}
+        };
+        int[][] answers = {
+                {3},
+                {1, 2, 3},
+                {0, 3, 4, 6, 9},
+                {-1, 7, 8, 12},
+                {-6, -10, -10, -18}
+        };
+
+        assertEquals(inputs.length, answers.length);
+
+        for (int i = 0; i < inputs.length; i++) {
+            for (int j = 0; j < inputs[i].length; j++) {
+                int expected = answers[i][j];
+                Problem2.bubbleSort(inputs[i]);
+                int actual = inputs[i][j];
+
+                assertEquals(expected, actual);
+            }
+
+        }
     }
 }
